db2f12a0fef5388cf91783801898ded6b069d89d
DmytroBozhor <dimaastop1@gmail.com>
2023-10-07 13:51:15 +0200

    spring security custom login page, authorization, logout basic config

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -20,12 +24,34 @@ public class SecurityConfig {
         this.personService = personService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //authentication
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personService);
     }
 
+    //custom login page + authorization + logout
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/auth/login", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/hello", true)
+                        .failureUrl("/auth/login?error")
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/com/example/springsecurity/controller/MainController.java
+++ src/main/java/com/example/springsecurity/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @GetMapping
+    @GetMapping("/hello")
     @ResponseBody
     public String hello(){
         return "hello";
